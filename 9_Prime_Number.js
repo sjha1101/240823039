bb0646de4f236fe4416d3fa11d24980dfc97f050
Sjha1101 <codingchannel8i@gmail.com>
2025-07-29 13:14:29 +0530

    change made to prime number

--- 9_Prime_Number.js
+++ 9_Prime_Number.js
@@ -1,8 +1,18 @@
 //To check the number is prime or not
 var a = 11;
+j=0;
+pri=0;
+d=0;
+for (let i=1;i<=a;i++) {
+    if (a % i == 0) {
+        console.log(i)
+        d++
+    }
+}
+console.log(d)
 
-if(a % 2 != 0){
-    console.log("Number is prime number");
+if (d<=2 && pri <= a) {
+    console.log("It is an prime number.");
 }else{
-    console.log("Number is not a prime number");
+    console.log("It is not an prime number.");
 }
--- server_environment/index.js
+++ server_environment/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 app.get('/',(req,res)=>{
-    res.send('Hi khushali')
+    res.send('Hello')
 });
 app.listen(80,()=>{
     console.log('server is running at http://localhost:80')
