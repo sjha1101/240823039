0465d0014352626467b26efb77f3f530e77fc216
Sjha1101 <codingchannel8i@gmail.com>
2025-07-29 12:14:48 +0530

    change made to see the file is running on the ip address

--- server_environment/index.js
+++ server_environment/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 app.get('/',(req,res)=>{
-    res.send('Hello')
+    res.send('Hi khushali')
 });
 app.listen(80,()=>{
     console.log('server is running at http://localhost:80')
